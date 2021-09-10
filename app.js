08d7d9e2fc0609537f1e2ce6801c8715da57aa61
Brian Salazar Sanchez <bsalazar@MacBook-Pro-de-Brian.local>
2021-09-09 21:17:18 -0600

    set eslint and prettier

--- app.js
+++ app.js
@@ -6,10 +6,15 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE',
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
 });
+
+app.get('/', (req, res) => res.send({ hello: 'World' }));
 
 app.listen(8080);
