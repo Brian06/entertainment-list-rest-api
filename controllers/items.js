f85699edd13e2ec9ab55d1243d09e339b14e3c71
Brian Salazar Sanchez <bsalazar@MacBook-Pro-de-Brian.local>
2021-10-06 20:17:16 -0600

    refactor to use async await

--- controllers/items.js
+++ controllers/items.js
@@ -1,27 +1,26 @@
 const { validationResult } = require('express-validator');
 const Item = require('../models/item');
 
-exports.getItems = (req, res, next) => {
-  Item.find()
-    .then(items => {
-      res.status(200).json({ message: 'Fetched Items successfully', items });
-    })
-    .catch(err => {
-      const error = err;
-      if (!error.statusCode) {
-        error.statusCode = 500;
-      }
-      next(error);
-    });
+exports.getItems = async (req, res, next) => {
+  try {
+    const items = await Item.find();
+    res.status(200).json({ message: 'Fetched Items successfully', items });
+  } catch (err) {
+    const error = err;
+    if (!error.statusCode) {
+      error.statusCode = 500;
+    }
+    next(error);
+  }
 };
 
-exports.createItem = (req, res, next) => {
+exports.createItem = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     const error = new Error('Validations fails, entered data is incorrect');
     error.statusCode = 422;
     error.errors = errors.array();
-    throw error;
+    next(error); // TODO: rearch why was an error to have throw error
   }
   const { title, type, description, durationMinutes, episodes, genres, imgURL } = req.body;
 
@@ -35,41 +34,38 @@ exports.createItem = (req, res, next) => {
     imgURL,
   });
 
-  item
-    .save()
-    .then(result => {
-      res.status(201).json({
-        message: 'Item created successfully',
-        item: result,
-      });
-    })
-    .catch(err => {
-      const error = err;
-      if (!error.statusCode) {
-        error.statusCode = 500;
-      }
-      next(error);
+  try {
+    const result = await item.save();
+    res.status(201).json({
+      message: 'Item created successfully',
+      item: result,
     });
+  } catch (err) {
+    const error = err;
+    if (!error.statusCode) {
+      error.statusCode = 500;
+    }
+    next(error);
+  }
 };
 
-exports.getItem = (req, res, next) => {
+exports.getItem = async (req, res, next) => {
   const { itemId } = req.params;
-  Item.findById(itemId)
-    .then(item => {
-      if (!item) {
-        const error = new Error('Could not find a post');
-        error.statusCode = 404;
-        throw error;
-      }
-      res.status(200).json({ message: 'Item fetched', item });
-    })
-    .catch(err => {
-      const error = err;
-      if (!error.statusCode) {
-        error.statusCode = 500;
-      }
+  try {
+    const item = await Item.findById(itemId);
+    if (!item) {
+      const error = new Error('Could not find a post');
+      error.statusCode = 404;
       next(error);
-    });
+    }
+    res.status(200).json({ message: 'Item fetched', item });
+  } catch (err) {
+    const error = err;
+    if (!error.statusCode) {
+      error.statusCode = 500;
+    }
+    next(error);
+  }
 };
 
 // TODO refactor the catch code to a single function
