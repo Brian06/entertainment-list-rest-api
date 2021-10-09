97bd345c1455cedca151b4b194c3c403f941ac79
Brian Salazar Sanchez <bsalazar@MacBook-Pro-de-Brian.local>
2021-10-09 11:55:03 -0600

    add update item endpoint

--- controllers/items.js
+++ controllers/items.js
@@ -15,26 +15,26 @@ exports.getItems = async (req, res, next) => {
 };
 
 exports.createItem = async (req, res, next) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    const error = new Error('Validations fails, entered data is incorrect');
-    error.statusCode = 422;
-    error.errors = errors.array();
-    next(error); // TODO: rearch why was an error to have throw error
-  }
-  const { title, type, description, durationMinutes, episodes, genres, imgURL } = req.body;
-
-  const item = new Item({
-    title,
-    type,
-    description,
-    durationMinutes,
-    episodes,
-    genres,
-    imgURL,
-  });
-
   try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const error = new Error('Validations fails, entered data is incorrect');
+      error.statusCode = 422;
+      error.errors = errors.array();
+      throw error; // TODO: rearch why was an error to have throw error
+    }
+    const { title, type, description, durationMinutes, episodes, imgURL, genres } = req.body;
+
+    const item = new Item({
+      title,
+      type,
+      description,
+      durationMinutes,
+      episodes,
+      genres,
+      imgURL,
+    });
+
     const result = await item.save();
     res.status(201).json({
       message: 'Item created successfully',
@@ -54,11 +54,51 @@ exports.getItem = async (req, res, next) => {
   try {
     const item = await Item.findById(itemId);
     if (!item) {
-      const error = new Error('Could not find a post');
+      const error = new Error('Could not find a item');
       error.statusCode = 404;
-      next(error);
+      throw error;
     }
     res.status(200).json({ message: 'Item fetched', item });
+  } catch (err) {
+    const error = err;
+    if (!error.statusCode) {
+      error.statusCode = 500;
+    }
+    next(error);
+  }
+};
+
+exports.updateItem = async (req, res, next) => {
+  try {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      const error = new Error('Validations fails, entered data is incorrect');
+      error.statusCode = 422;
+      error.errors = errors.array();
+      throw error; // TODO: rearch why was an error to have throw error
+    }
+
+    const { itemId } = req.params;
+    const { title, type, description, durationMinutes, episodes, imgURL, genres } = req.body;
+
+    const item = await Item.findById(itemId);
+    if (!item) {
+      const error = new Error('Could not find a item');
+      error.statusCode = 404;
+      throw error;
+    }
+
+    item.title = title;
+    item.type = type;
+    item.description = description;
+    item.durationMinutes = durationMinutes;
+    item.episodes = episodes;
+    item.imgURL = imgURL;
+    item.genres = genres;
+
+    const updatedItem = await item.save();
+    res.status(200).json({ message: 'Item Updated', updatedItem });
   } catch (err) {
     const error = err;
     if (!error.statusCode) {
